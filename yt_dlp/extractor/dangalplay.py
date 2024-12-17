141263ba0258f2ffd8118fbc467b0b298568970f
aryanchy451 <pht449@gmail.com>
2024-12-17 19:57:18 +0530

    Update dangalplay.py

--- yt_dlp/extractor/dangalplay.py
+++ yt_dlp/extractor/dangalplay.py
@@ -60,7 +60,7 @@ class DangalPlayBaseIE(InfoExtractor):
 
 class DangalPlayIE(DangalPlayBaseIE):
     IE_NAME = 'dangalplay'
-    _VALID_URL = r'https?://(?:www\.)?dangalplay.com/(shows|movies)/(?P<series>[^/?#]+)/(?P<id>(?!episodes)[^/?#]+)/?(?:$|[?#])'    
+    _VALID_URL = r'https?://(?:www\.)?dangalplay\.com/(shows|movies)/(?:(?P<series>[^/?#]+)/(?P<id>[^/?#]+)|[^/?#]+)'
     _TESTS = [{
         'url': 'https://www.dangalplay.com/shows/kitani-mohabbat-hai-season-2/kitani-mohabbat-hai-season-2-ep-number-01',
         'info_dict': {
